045ed9d69c528834316be24b7b1c6eca8c81c5c6
Seymur03082001 <tu2e4fm6u@code.edu.az>
2022-12-22 03:26:52 +0400

    first commit

---
+++ ADONET/Program.cs
@@ -0,0 +1,14 @@
+﻿using ADONET.Models;
+
+namespace ADONET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Music musics= new Music();
+            Artist artists = new Artist();
+            Console.WriteLine("Eminem-Till I Collapse");
+        }
+    }
+}
